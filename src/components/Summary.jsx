5b35b9350d36616bcbd0015bc844b67253282271
Ketul Patel <ketulp19120@gmail.com>
2026-07-21 19:45:48 -0400

    - debugging javaScript  error on Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -8,10 +8,7 @@ import { summary } from '../data';
 export default function Summary() {
   return (
     <SectionWrapper id="summary">
-      <div className={styles.sectionLabel}></div>
-      <h2 className={styles.sectionTitle}>Who I Am</h2>
-      <div className={sectionStyles.divider}></div>
-      <p className={own.text}>{summary}</p>
+      
     </SectionWrapper>
   );
 }
