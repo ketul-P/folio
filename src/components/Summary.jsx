0e5c8cf24863eadfa6b4ff4df46a496fd4df6fcd
Ketul Patel <ketulp19120@gmail.com>
2026-07-21 19:39:25 -0400

    - fixed import error in summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,5 +1,7 @@
 import SectionWrapper from './SectionWrapper';
 import sectionStyles from './SectionWrapper.module.css';
+import sectionLabel from './SectionWrapper.module.css';
+import sectionTitle from './SectionWrapper.module.css';
 import own from './Summary.module.css';
 import { summary } from '../data';
 
